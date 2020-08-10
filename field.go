7c2e129ac013bf87865baae3446143efd4d05be1
tdakkota <tanc13@yandex.ru>
2020-08-10 07:30:52 +0300

    feat: add field builders

---
+++ field.go
@@ -0,0 +1,25 @@
+package builders
+
+import "go/ast"
+
+// Field builds struct field.
+func Field(names ...*ast.Ident) func(typ ast.Expr, tag *ast.BasicLit) *ast.Field {
+	return func(typ ast.Expr, tag *ast.BasicLit) *ast.Field {
+		return &ast.Field{
+			Names: names,
+			Type:  typ,
+			Tag:   tag,
+		}
+	}
+}
+
+// Param builds function parameter.
+func Param(names ...*ast.Ident) func(typ ast.Expr) *ast.Field {
+	return func(typ ast.Expr) *ast.Field {
+		return &ast.Field{
+			Names: names,
+			Type:  typ,
+			Tag:   nil,
+		}
+	}
+}
